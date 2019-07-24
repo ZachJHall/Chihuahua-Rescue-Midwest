db609936efb0334d1d3f9cac503ebf5f2f2e1a22
cjwagn1 <37089795+cjwagn1@users.noreply.github.com>
2019-07-24 06:59:10 -0500

    Organized code

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../CSS/footer.css";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="footer">
+          <p>footer</p>
+        </div>
+      </footer>
+    );
+  }
+}
