9e973d45aaff9b150084891eb7f1c40591071ac0
Carter Wagner <cjwagn1@gmail.com>
2019-09-28 15:11:54 -0500

    slideshow

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Carousel } from "antd";
+import "antd/dist/antd.css";
+import "../CSS/home.css";
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>home</h1>
-      </div>
+      <Carousel autoplay>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+      </Carousel>
     );
   }
 }
