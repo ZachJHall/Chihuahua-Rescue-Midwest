9c219baa350f4c89853db1be42200eb369b18bf6
cjwagn1 <37089795+cjwagn1@users.noreply.github.com>
2019-10-05 19:04:56 -0500

    Updates

---
+++ precache-manifest.b4c2f12b4521a26a31d25957021acfa7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b89f85e80ef1623e6bda92b759d6807",
+    "url": "/Chihuahua-Rescue-Midwest/index.html"
+  },
+  {
+    "revision": "2472ab844a514e69efca",
+    "url": "/Chihuahua-Rescue-Midwest/static/css/main.4557b9d7.chunk.css"
+  },
+  {
+    "revision": "cec9b57a58dd526156fa",
+    "url": "/Chihuahua-Rescue-Midwest/static/js/2.3f52fc08.chunk.js"
+  },
+  {
+    "revision": "2472ab844a514e69efca",
+    "url": "/Chihuahua-Rescue-Midwest/static/js/main.2ff8702b.chunk.js"
+  },
+  {
+    "revision": "886b9c9f0569acb9ce9d",
+    "url": "/Chihuahua-Rescue-Midwest/static/js/runtime~main.f6d697f7.js"
+  },
+  {
+    "revision": "d727bff7e5978a4e2badc28fc11b8b92",
+    "url": "/Chihuahua-Rescue-Midwest/static/media/dog.d727bff7.png"
+  },
+  {
+    "revision": "ca1048b706ecf51efc6b4780f7c04c11",
+    "url": "/Chihuahua-Rescue-Midwest/static/media/dog1.ca1048b7.jpg"
+  },
+  {
+    "revision": "fd12aa5db09d409492ce2ee0f81f2be6",
+    "url": "/Chihuahua-Rescue-Midwest/static/media/dog2.fd12aa5d.jpg"
+  },
+  {
+    "revision": "f6706290135fd3e032f3c3bf8f1b9022",
+    "url": "/Chihuahua-Rescue-Midwest/static/media/dog3.f6706290.jpg"
+  }
+]);
