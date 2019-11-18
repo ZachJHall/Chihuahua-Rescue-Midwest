5ff39c8815d3bb468d5b6ce898b28c19bb67f467
cjwagn1 <37089795+cjwagn1@users.noreply.github.com>
2019-11-18 00:27:21 -0600

    Updates

---
+++ precache-manifest.6cd0f496ed276a67fe58460963103256.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4611632205b81965188a066f706e7bee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55dd86f7181a06662788",
+    "url": "/static/css/main.1affba1f.chunk.css"
+  },
+  {
+    "revision": "804640dea73c2daf277b",
+    "url": "/static/js/2.6a6d1b03.chunk.js"
+  },
+  {
+    "revision": "55dd86f7181a06662788",
+    "url": "/static/js/main.10d2d237.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "33d9254ec99034aa41e800977e87c1d7",
+    "url": "/static/media/barnaby1.33d9254e.jpg"
+  },
+  {
+    "revision": "21befa53bb86746c821593cd1eaaf8bb",
+    "url": "/static/media/chi1.21befa53.jpg"
+  },
+  {
+    "revision": "68941e4abfbf461cb6daebdd72d9aabb",
+    "url": "/static/media/chi2.68941e4a.jpeg"
+  },
+  {
+    "revision": "51c7b9e75dfb11739b5fe01c033b5c93",
+    "url": "/static/media/chi3.51c7b9e7.jpeg"
+  },
+  {
+    "revision": "b78630d0ba87dfdd792f62feede14129",
+    "url": "/static/media/dog.b78630d0.png"
+  },
+  {
+    "revision": "e2b00a0c196129d9882dacb73a0abe56",
+    "url": "/static/media/jeannie1.e2b00a0c.jpeg"
+  },
+  {
+    "revision": "4615b70c692a28add38936dcdfd20d58",
+    "url": "/static/media/jed1.4615b70c.jpg"
+  },
+  {
+    "revision": "513036fe4193cb3c73bcb87f1a43b9b7",
+    "url": "/static/media/luna1.513036fe.jpg"
+  },
+  {
+    "revision": "a1e4fda67971aad918f542bb0af120a4",
+    "url": "/static/media/oliver1.a1e4fda6.jpg"
+  },
+  {
+    "revision": "05a8aa12b561ae00278f4562fffdbcfe",
+    "url": "/static/media/oreo1.05a8aa12.jpg"
+  },
+  {
+    "revision": "f7aa8b81b0c8f76b0ff913c73a283048",
+    "url": "/static/media/pixie1.f7aa8b81.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Chihuahua-Rescue-Midwest/precache-manifest.f8481bf1931b29e5ba87ed131165a630.js"
+  "/precache-manifest.6cd0f496ed276a67fe58460963103256.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Chihuahua-Rescue-Midwest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
